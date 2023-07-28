a84287f473ef474fe9a5621cc59e076b28079e17
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-07-28 16:49:39 -0400

    added projects, fixed a little css

--- proj/src/home.js
+++ proj/src/home.js
@@ -2,8 +2,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className='homepage' >
-      <h1>Welcome </h1>
-      <h2> My name is Yulia</h2>
+      <h1> Hello, I'm Yulia</h1>
       <h2> Get to know me and my coding journey</h2>
     </div>
   );
--- proj/src/projects.js
+++ proj/src/projects.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import React from "react";
+import Card from "react-bootstrap/Card";
 const Projects = () => {
   return (
-    <div className='projects' >
-      <h2>WordFinder</h2>
-      <h2>Movies-n-Me</h2>
-      <h2>Resunet</h2>
-      <h2>Pizza Assignment</h2>
-      <h2>MoneyTrack</h2>
-      <h2>Random dish generator</h2>
-
+    <div className="projects">
+      <h1>My Projects</h1>
+      <br></br>
+      <div className="cardStyle">
+        <Card>
+          <Card.Header as="h5">Personal</Card.Header>
+          <Card.Body>
+            <Card.Title>Resunet</Card.Title>
+            <Card.Text>c#</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="cardStyle">
+        <Card >
+          <Card.Header as="h5">Personal</Card.Header>
+          <Card.Body>
+            <Card.Title>moneyTrack</Card.Title>
+            <Card.Text>Track server income, tips plus hourly</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="cardStyle">
+        <Card >
+          <Card.Header as="h5">Personal</Card.Header>
+          <Card.Body>
+            <Card.Title>Random meal generator</Card.Title>
+            <Card.Text>Generate proteins, garnishes, or full meals </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="cardStyle">
+        <Card >
+          <Card.Header as="h5">College</Card.Header>
+          <Card.Body>
+            <Card.Title>WordFinder</Card.Title>
+            <Card.Text>FInal React based Dictionary and Thesaurus App working with API</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="cardStyle">
+        <Card >
+          <Card.Header as="h5">College</Card.Header>
+          <Card.Body>
+            <Card.Title>Movies-n-me</Card.Title>
+            <Card.Text>ReactNative final project working with IMDB API to search and save movies</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="cardStyle">
+        <Card >
+          <Card.Header as="h5">College</Card.Header>
+          <Card.Body>
+            <Card.Title>Pizza Assignment</Card.Title>
+            <Card.Text>Interactive project on JavaFX. Choose pizza toppings, size, quantity</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 };
-  
+
 export default Projects;
