ef71e13514666d300117125cf4ee5960a15c16aa
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-07-27 22:01:17 -0400

    done basic pages,navbar

--- proj/src/about.js
+++ proj/src/about.js
@@ -1,16 +1,8 @@
 import React from 'react';
-  
 const About = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>About</h1>
+    <div className='about' >
+      <p>text</p>
     </div>
   );
 };
--- proj/src/projects.js
+++ proj/src/projects.js
@@ -1,16 +1,14 @@
 import React from 'react';
-  
 const Projects = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>Projects</h1>
+    <div className='projects' >
+      <h2>WordFinder</h2>
+      <h2>Movies-n-Me</h2>
+      <h2>Resunet</h2>
+      <h2>Pizza Assignment</h2>
+      <h2>MoneyTrack</h2>
+      <h2>Random dish generator</h2>
+
     </div>
   );
 };
