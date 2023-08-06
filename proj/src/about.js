75a43445d68088cf5dbf05ac042c0bb76c4628f7
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-08-06 15:51:36 -0400

    adjusted css for section about and proj

--- proj/src/about.js
+++ proj/src/about.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React from "react";
 const About = () => {
   return (
-    <div id='About' className='about' >
-      <p>My coding experience began back in grade 10, when I took a computer engineering course. I quickly figured out that hardware was not for me, but the coding part of getting a robot to avoid light, attack or turn around when hit a wall is much more interseting. I then took a coding course, working with Java. As a final project working with Java and GUI, i remade my parents childhood game of a wolf catching eggs. 
-
-i took the engeneering course once again where as a team we build an automated animal feader. Building the frame from screacth, using motors, arduino, and making an app to control the feeder via blootooth. 
-After highschool I chose to go into Police Foundations, but found myself working on the game in borning lectures. I decided to finish the diploma and go back to school for computer programming. 
-
-I am currently in my last year of computer progamming at Humber College. Humber gave me a feel for Java, SpringBoot, React, ReactNative, manual and automated testing. As well, a feel for project management, working with agile and waterfall methedologies, and undersetanding how to work with SCRUM. 
-
-Putting everything Humber has given me I am working on personal projects to practice React, and ReacNative. I also very curious of learning more about C# and .Net.</p>
+    <div id="About" className="about">
+      <div className="About-Header">
+      <h1>About me</h1>
+      </div>
+      <p>
+        My coding experience began back in grade 10, when I took a computer
+        engineering course. I quickly figured out that hardware was not for me,
+        but the coding part of getting a robot to avoid light, attack or turn
+        around when hit a wall is much more interseting. I then took a coding
+        course, working with Java. As a final project working with Java and GUI,
+        i remade my parents childhood game of a wolf catching eggs.
+      </p>
+      <p>
+        i took the engeneering course once again where as a team we build an
+        automated animal feader. Building the frame from screacth, using motors,
+        arduino, and making an app to control the feeder via blootooth. After
+        highschool I chose to go into Police Foundations, but found myself
+        working on the game in borning lectures. I decided to finish the diploma
+        and go back to school for computer programming.{" "}
+      </p>
+      <p>
+        I am currently in my last year of computer progamming at Humber College.
+        Humber gave me a feel for Java, SpringBoot, React, ReactNative, manual
+        and automated testing. As well, a feel for project management, working
+        with agile and waterfall methedologies, and undersetanding how to work
+        with SCRUM.{" "}
+      </p>
+      <p>
+        Putting everything Humber has given me I am working on personal projects
+        to practice React, and ReacNative. I also very curious of learning more
+        about C# and.Net.
+      </p>
     </div>
   );
 };
-  
+
 export default About;
